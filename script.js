bbe996a7052f216e0baf208e1db7326234fef06a
Liv <lopez.olivia17@outlook.com>
2022-01-06 13:55:04 -0600

    tried adding function to pull user input and place it in the myLibrary array. Adjusted HTML accordingly.

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+let addBtn = document.getElementById('add-book-btn');
+
 let myLibrary = [];
 
 function Book() {
   // the constructor 
 }
 
+// add a function to take user’s input and store the myLibrary array
+
 function addBookToLibrary() {
-  // do stuff here 
+  // grab values from inputs 
+  // let titleInput = document.getElementById('title-input').value;
+  // let authorInput = document.getElementById('author-input').value;
+ 
+  myLibrary.push(document.getElementById('title-input').value)
 }
+
+console.log(myLibrary)
+
+
+addBtn.addEventListener('click', addBookToLibrary)
